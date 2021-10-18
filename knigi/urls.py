22d38b5b342fa6f3be4cbbe793893fd86e2fd172
Alex B <ab@icash.ru>
2021-10-18 22:26:38 +0300

    index

--- knigi/urls.py
+++ knigi/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path, include
+from app import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('', views.Home, name='home'), 
+
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
