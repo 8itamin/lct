b8bc95df9926edbdf483dedf7c1439e5bc1b16c2
Alex B <ab@icash.ru>
2021-10-18 23:02:53 +0300

    api

--- app/views.py
+++ app/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
 
 def Home(request):
+    """
+    Template - Index page
+    """
     return render(request, 'app/index.html', context = {'username': request.user, })
+
+def my_api_view(request):
+    """
+    Request page:
+    in - Json ID 
+    out - Json
+            recommendations (id, title, author),
+            history (id, title, author) 
+    """
+    data = {
+        'recommendations': {
+            'id': '789',
+            'title': 'Красная шапочка',
+            'author': 'Пьерро',
+            },
+        'history': {
+            'id': '123',
+            'title': 'Незнайка на луне',
+            'author': 'Носов',
+            }
+    }
+    return JsonResponse(data)
